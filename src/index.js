62fe14ae917a2244eafb795bbf0c1d3acf5bbbea
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-03-23 16:22:30 +0200

    Оновлює файли

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import './sass/index.scss';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './js/fetchImages';
+import { renderGallery } from './js/renderGallery';
+
+const searchFormRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+const loadMoreRef = document.querySelector('.load-btn');
+const PER_PAGE = 40;
+let searchQuery = '';
+let page = 1;
+let lightBox;
+
+searchFormRef.addEventListener('submit', onSearchForm);
+loadMoreRef.addEventListener('click', onLoadMoreClick);
+
+function onSearchForm(e) {
+  e.preventDefault();
+  galleryRef.innerHTML = '';
+  loadMoreRef.classList.add('is-hidden');
+
+  searchQuery = e.currentTarget.searchQuery.value;
+  page = 1;
+
+  if (!searchQuery) {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
+    );
+    return;
+  }
+
+  fetchImages(searchQuery, page)
+    .then(({ data }) => {
+      if (data.totalHits === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderGallery(data.hits);
+        lightBox = new SimpleLightbox('.gallery a', {
+          captionsData: 'alt',
+          captionDelay: 250,
+        }).refresh();
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+        if (data.totalHits > PER_PAGE) {
+          loadMoreRef.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      searchFormRef.reset();
+    });
+}
+
+function onLoadMoreClick() {
+  page += 1;
+  lightBox.destroy();
+
+  fetchImages(searchQuery, page)
+    .then(({ data }) => {
+      renderGallery(data.hits);
+      lightBox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      }).refresh();
+
+      const totalPages = Math.ceil(data.totalHits / PER_PAGE);
+
+      if (page === totalPages) {
+        loadMoreRef.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
